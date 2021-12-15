63c9dac01536e845cd6dadda49dd5c3417480306
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2021-12-15 16:21:04 +0900

    [add] crcgen

---
+++ crcgen/Readme.md
@@ -0,0 +1,13 @@
+
+## Download crcgen
+
+```console
+shell$ git clone https://github.com/mbuesch/crcgen
+```
+
+## Build
+
+```console
+shell$ make build
+```
+
---
+++ crcgen/scripts/build.mk
@@ -0,0 +1,51 @@
+PROJECT_NAME   ?= crc32_0032
+NR_DATA_BITS   ?= 32
+DEVICE_NAME    ?= "xczu3eg-sbva484-1-e"
+CLOCK_PERIOD   ?= 1.0
+
+PYTHON         ?= python3.7
+CRCGEN         ?= $(PYTHON) ../crcgen/crcgen
+TOPGEN         ?= $(PYTHON) ../scripts/topgen.py
+VIVADO         ?= vivado
+
+CRC_NAME       := $(PROJECT_NAME)
+TOP_NAME       := $(PROJECT_NAME)_top
+DATA_PARAM     := d
+CRC_IN_PARAM   := c
+CRC_OUT_PARAM  := o
+
+TARGET         := $(PROJECT_NAME).rpt
+SOURCES        := build.tcl parameter.tcl timing.xdc $(CRC_NAME).vhd $(TOP_NAME).vhd
+
+sources: $(SOURCES)
+
+build: $(TARGET)
+
+clean:
+	-rm $(SOURCES)
+
+parameter.tcl :
+	echo "set data_width    " $(NR_DATA_BITS) >  $@
+	echo "set crc_name      " $(CRC_NAME)     >> $@
+	echo "set top_name      " $(TOP_NAME)     >> $@
+	echo "set device_part   " $(DEVICE_NAME)  >> $@
+	echo "set project_name  " $(PROJECT_NAME) >> $@
+
+timing.xdc:
+	echo "create_clock -period " $(CLOCK_PERIOD) " -name CLK [get_port CLK]" > $@
+
+build.tcl :
+	echo 'set     project_directory   [file dirname [info script]]' >  $@
+	echo 'source                      [file join $$project_directory "parameter.tcl"]' >> $@
+	echo 'lappend constrs_file_list   [file join $$project_directory "timing.xdc"]'    >> $@
+	echo 'source                      [file join $$project_directory ".." "scripts" "create_project.tcl"]' >> $@
+	echo 'source                      [file join $$project_directory ".." "scripts" "implementation.tcl"]' >> $@
+
+$(CRC_NAME).vhd :
+	$(CRCGEN) -V -a CRC-32 -b $(NR_DATA_BITS) -n $(CRC_NAME) -D d -C c -o o > $@
+
+$(TOP_NAME).vhd :
+	$(TOPGEN) -n $(CRC_NAME) -b $(NR_DATA_BITS) > $@
+
+$(PROJECT_NAME).rpt : $(SOURCES)
+	$(VIVADO) -mode batch -source build.tcl
