4aaa90bc8171a089128fa692d38512c435e5fc67
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2021-12-21 08:55:30 +0900

    [add] opttbl

---
+++ opttbl/scripts/create_project.tcl
@@ -0,0 +1,108 @@
+#
+# create_project.tcl  Tcl script for creating project
+#
+if {[info exists top_name    ] == 0} {
+    puts "ERROR: Please set top_name."
+    return 1
+}
+if {[info exists data_width  ] == 0} {
+    puts "ERROR: Please set data_width."
+    return 1
+}
+if {[info exists project_name     ] == 0} {
+    set project_name        "project"
+}
+if {[info exists project_directory] == 0} {
+    set project_directory   [pwd]
+}
+#
+# Create Project
+#
+create_project -force $project_name $project_directory
+#
+# Set project properties
+#
+if       {[info exists board_part ] && [string equal $board_part  "" ] == 0} {
+    set_property "board_part"     $board_part      [current_project]
+} elseif {[info exists device_part] && [string equal $device_part "" ] == 0} {
+    set_property "part"           $device_part     [current_project]
+} else {
+    puts "ERROR: Please set board_part or device_part."
+    return 1
+}
+set_property "default_lib"        "xil_defaultlib" [current_project]
+set_property "simulator_language" "Mixed"          [current_project]
+set_property "target_language"    "VHDL"           [current_project]
+#
+# Create fileset "sources_1"
+#
+if {[string equal [get_filesets -quiet sources_1] ""]} {
+    create_fileset -srcset sources_1
+}
+#
+# Create fileset "constrs_1"
+#
+if {[string equal [get_filesets -quiet constrs_1] ""]} {
+    create_fileset -constrset constrs_1
+}
+#
+# Create fileset "sim_1"
+#
+if {[string equal [get_filesets -quiet sim_1] ""]} {
+    create_fileset -simset sim_1
+}
+#
+# Create run "synth_1" and set property
+#
+set synth_1_flow     "Vivado Synthesis 2020"
+set synth_1_strategy "Vivado Synthesis Defaults"
+if {[string equal [get_runs -quiet synth_1] ""]} {
+    create_run -name synth_1 -flow $synth_1_flow -strategy $synth_1_strategy -constrset constrs_1
+} else {
+    set_property flow     $synth_1_flow     [get_runs synth_1]
+    set_property strategy $synth_1_strategy [get_runs synth_1]
+}
+current_run -synthesis [get_runs synth_1]
+#
+# Create run "impl_1" and set property
+#
+set impl_1_flow      "Vivado Implementation 2020"
+set impl_1_strategy  "Performance_Explore"
+if {[string equal [get_runs -quiet impl_1] ""]} {
+    create_run -name impl_1 -flow $impl_1_flow -strategy $impl_1_strategy -constrset constrs_1 -parent_run synth_1
+} else {
+    set_property flow     $impl_1_flow      [get_runs impl_1]
+    set_property strategy $impl_1_strategy  [get_runs impl_1]
+}
+current_run -implementation [get_runs impl_1]
+#
+# Set IP Repository
+#
+if {[info exists ip_repo_path_list] && [llength $ip_repo_path_list] > 0 } {
+    set_property ip_repo_paths $ip_repo_path_list [current_fileset]
+    update_ip_catalog
+}
+#
+# Import Constraint files
+#
+if {[info exists constrs_file_list] && [llength $constrs_file_list] > 0 } {
+    add_files    -fileset constrs_1 -norecurse $constrs_file_list
+}
+#
+# Add VHDL Files
+#
+proc add_vhdl_file {fileset_name library_name file_name} {
+    set file    [file normalize $file_name]
+    set fileset [get_filesets   $fileset_name] 
+    add_files -norecurse -fileset $fileset $file
+    set file_obj [get_files -of_objects $fileset $file]
+    set_property "file_type" "VHDL"        $file_obj
+    set_property "library"   $library_name $file_obj
+}
+add_vhdl_file sources_1 "WORK"  [file join $project_directory ".." "src" "crc_gen.vhd"]
+add_vhdl_file sources_1 "WORK"  [file join $project_directory "$top_name.vhd"]
+set_property top $top_name [get_filesets sources_1]
+#
+# Close Project
+#
+close_project
---
+++ opttbl/src/crc_gen.vhd
@@ -0,0 +1,210 @@
+-----------------------------------------------------------------------------------
+--!     @file    crcgen.vhd
+--!     @brief   CRCGEN : あらかじめ最適化されたテーブルを用いて CRC 演算するモジュール
+--!     @version 0.0.1
+--!     @date    2021/12/20
+--!     @author  Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
+-----------------------------------------------------------------------------------
+--
+--      Copyright (C) 2021 Ichiro Kawazome
+--      All rights reserved.
+--
+--      Redistribution and use in source and binary forms, with or without
+--      modification, are permitted provided that the following conditions
+--      are met:
+--
+--        1. Redistributions of source code must retain the above copyright
+--           notice, this list of conditions and the following disclaimer.
+--
+--        2. Redistributions in binary form must reproduce the above copyright
+--           notice, this list of conditions and the following disclaimer in
+--           the documentation and/or other materials provided with the
+--           distribution.
+--
+--      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+--      "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+--      LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+--      A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+--      OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+--      SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+--      LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+--      DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+--      THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+--      (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+--      OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--
+-----------------------------------------------------------------------------------
+library ieee;
+use     ieee.std_logic_1164.all;
+entity  CRC_GEN is
+    generic (
+        CRC_BITS  : integer := 32;
+        CRC_POLY  : integer := 16#04C11DB7#;
+        CRC_INIT  : integer := 1;
+        DATA_BITS : integer := 32
+    );
+    port (
+        CLK       : in  std_logic;
+        RST       : in  std_logic;
+        LOAD      : in  std_logic;
+        LAST      : in  std_logic;
+        DATA      : in  std_logic_vector(DATA_BITS-1 downto 0);
+        CRC       : out std_logic_vector(CRC_BITS -1 downto 0);
+        VAL       : out std_logic
+    );
+end entity;
+
+library ieee;
+use     ieee.std_logic_1164.all;
+use     ieee.numeric_std.all;
+architecture RTL of CRC_GEN is
+    -------------------------------------------------------------------------------
+    -- CRC_BITS の１次元配列のタイプ宣言
+    -------------------------------------------------------------------------------
+    subtype   CRC_TYPE          is std_logic_vector(CRC_BITS-1 downto 0);
+    signal    curr_crc          :  CRC_TYPE;
+    function  CONV_CRC_TYPE(N:integer) return CRC_TYPE is
+    begin
+        return std_logic_vector(TO_UNSIGNED(N,CRC_BITS));
+    end CONV_CRC_TYPE;
+    -------------------------------------------------------------------------------
+    -- CRC用多項式(CRC_POLY)が整数のままだと使いにくいので、ビット配列に変換しておく
+    -------------------------------------------------------------------------------
+    constant  CRC_POLY_BIT      : CRC_TYPE := CONV_CRC_TYPE(CRC_POLY);
+    -------------------------------------------------------------------------------
+    -- CRCの初期値を計算する関数の定義
+    -------------------------------------------------------------------------------
+    function  CONV_CRC_INIT(CRC_INIT: integer) return CRC_TYPE is
+        variable crc_init_val   :  CRC_TYPE;
+    begin
+        if (CRC_INIT = 0) then
+            crc_init_val := (others => '0');
+        else
+            crc_init_val := (others => '1');
+        end if;
+        return crc_init_val;
+    end function;
+    -------------------------------------------------------------------------------
+    -- CRCの初期値
+    -------------------------------------------------------------------------------
+    constant  CRC_INIT_BIT      :  CRC_TYPE := CONV_CRC_INIT(CRC_INIT);
+    -------------------------------------------------------------------------------
+    -- １ビットの入力データに対してCRC演算する関数の定義
+    -------------------------------------------------------------------------------
+    function  CALC_CRC(CRC:CRC_TYPE;D:std_logic) return CRC_TYPE is
+        variable new_crc        :  CRC_TYPE;
+        variable crc_xor_d_bit  :  std_logic;
+        variable prev_crc_bit   :  std_logic;
+    begin
+        crc_xor_d_bit := CRC(CRC'high) xor D;
+        prev_crc_bit  := '0';
+        for i in 0 to CRC_BITS-1 loop
+            if (CRC_POLY_BIT(i) = '1') then
+                new_crc(i) := prev_crc_bit xor crc_xor_d_bit;
+            else
+                new_crc(i) := prev_crc_bit;
+            end if;
+            prev_crc_bit := CRC(i);
+        end loop;
+        return new_crc;
+    end function;
+    -------------------------------------------------------------------------------
+    -- 複数ビットまとめてCRC演算する関数の定義(自己最適化版)
+    --    簡易版では論理合成時にちゃんと最適化されない場合があるので、自分自身で最適化してしまう方法。
+    --    ただし、この方法は論理合成ツールが馬鹿だと、論理合成そのものに失敗してしまう可能性がある。
+    --    具体的には、あらかじめ xor するビットを演算しておいたテーブルを用意しておき、演算時にはそ
+    --    のテーブルを参照して、フラグの立っているところのみを xor する。
+    -------------------------------------------------------------------------------
+    --    あらかじめ演算しておくテーブルの型宣言
+    -------------------------------------------------------------------------------
+    type      CALC_CRC_OPT_BIT_TYPE is record
+                  CRC_BIT       : std_logic_vector( CRC_BITS-1 downto 0);
+                  DATA_BIT      : std_logic_vector(DATA_BITS-1 downto 0);
+    end record;
+    type      CALC_CRC_OPT_TABLE_TYPE is array(CRC_BITS-1 downto 0) of CALC_CRC_OPT_BIT_TYPE;
+    -------------------------------------------------------------------------------
+    --    テーブルの初期値を演算する関数の定義
+    -------------------------------------------------------------------------------
+    function  INIT_CALC_CRC_OPT_TABLE return CALC_CRC_OPT_TABLE_TYPE is
+        variable table : CALC_CRC_OPT_TABLE_TYPE;
+        variable carry : CALC_CRC_OPT_BIT_TYPE;
+    begin
+        for i in 0 to CRC_BITS-1 loop
+            for crc_pos in 0 to CRC_BITS-1 loop
+                if (crc_pos = i) then
+                    table(i).CRC_BIT(crc_pos) := '1';
+                else
+                    table(i).CRC_BIT(crc_pos) := '0';
+                end if;
+            end loop;
+            table(i).DATA_BIT := (others => '0');
+        end loop;
+        for i in 0 to DATA_BITS-1 loop
+            carry := table(CRC_BITS-1);
+            carry.DATA_BIT(i) := '1';
+            for crc_pos in CRC_BITS-1 downto 1 loop
+                table(crc_pos) := table(crc_pos-1);
+            end loop;
+            table(0).CRC_BIT  := (others => '0');
+            table(0).DATA_BIT := (others => '0');
+            for crc_pos in 0 to CRC_BITS-1 loop
+                if (CRC_POLY_BIT(crc_pos) = '1') then
+                    table(crc_pos).CRC_BIT  := table(crc_pos).CRC_BIT  xor carry.CRC_BIT;
+                    table(crc_pos).DATA_BIT := table(crc_pos).DATA_BIT xor carry.DATA_BIT;
+                end if;
+            end loop;
+        end loop;
+        return table;
+    end function;
+    -------------------------------------------------------------------------------
+    --    あらかじめ演算しておいたテーブルを定数として持っておく
+    -------------------------------------------------------------------------------
+    constant  CALC_CRC_OPT_TABLE : CALC_CRC_OPT_TABLE_TYPE := INIT_CALC_CRC_OPT_TABLE;
+    -------------------------------------------------------------------------------
+    --    あらかじめ演算しておいたテーブルに基づいて複数ビットの CRC 演算を行なう関数の定義
+    -------------------------------------------------------------------------------
+    function  CALC_CRC(CRC:CRC_TYPE;D:std_logic_vector) return CRC_TYPE is
+        variable new_crc       : CRC_TYPE;
+    begin
+        for i in 0 to CRC_BITS-1 loop
+            new_crc(i) := '0';
+            for pos in 0 to CRC_BITS-1 loop
+                if (CALC_CRC_OPT_TABLE(i).CRC_BIT(pos) = '1') then
+                    new_crc(i) := new_crc(i) xor CRC(pos);
+                end if;
+            end loop;
+            for pos in 0 to DATA_BITS-1 loop
+                if (CALC_CRC_OPT_TABLE(i).DATA_BIT(pos) = '1') then
+                    new_crc(i) := new_crc(i) xor D(D'low+pos);
+                end if;
+            end loop;
+        end loop;
+        return new_crc;
+    end function;
+begin
+    process(CLK, RST) 
+        variable next_crc :  CRC_TYPE;
+    begin
+        if (RST = '1') then
+            curr_crc <= CRC_INIT_BIT;
+            CRC      <= CRC_INIT_BIT;
+            VAL      <= '0';
+        elsif (CLK'event and CLK = '1') then
+            if (LOAD = '1') then
+                next_crc := CALC_CRC(curr_crc, DATA);
+                if (LAST = '1') then
+                    curr_crc <= CRC_INIT_BIT;
+                    CRC      <= next_crc;
+                    VAL      <= '1';
+                else
+                    curr_crc <= next_crc;
+                    CRC      <= next_crc;
+                    VAL      <= '0';
+                end if;
+            else
+                    VAL      <= '0';
+            end if;
+        end if;
+    end process;
+end RTL;
+
